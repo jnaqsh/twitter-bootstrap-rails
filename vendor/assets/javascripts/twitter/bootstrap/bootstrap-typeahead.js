92e591d53b11052b61d731feded4673dff0450c4
Hamed Ramezanian <iceage2098@gmail.com>
2012-11-18 11:08:14 +0330

    replace less and js files with rbootstrap project for rtl languages

--- vendor/assets/javascripts/twitter/bootstrap/bootstrap-typeahead.js
+++ vendor/assets/javascripts/twitter/bootstrap/bootstrap-typeahead.js
@@ -57,12 +57,13 @@
 
   , show: function () {
       var pos = $.extend({}, this.$element.offset(), {
-        height: this.$element[0].offsetHeight
+        height: this.$element[0].offsetHeight,
+        width: this.$element[0].offsetWidth
       })
 
       this.$menu.css({
         top: pos.top + pos.height
-      , left: pos.left
+      , right: pos.left - pos.width
       })
 
       this.$menu.show()
